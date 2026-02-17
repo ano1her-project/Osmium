0517c88cc31a69ca186c943805f90d1b6324a9af
ano1her-project <ano1her.project@gmail.com>
2026-02-17 19:32:54 +0100

    created interface project

---
+++ Osmium.Interface/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Osmium.Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void WaitForCommandAndReact()
+        {
+            string input = Console.ReadLine();
+        }
+    }
+}
